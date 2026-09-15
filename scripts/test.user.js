1dfaa9c020cf1837732af5c501c716952cc4a31b
espenraugstad <espen.raugstad@gmail.com>
2026-09-15 19:23:44 +0200

    Updated astro and added a test script

---
+++ scripts/test.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Simple test
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Tests
+// @author       Espen Raugstad
+// @match        https://www.vg.no
+// @updateURL    
+// @downloadURL  
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("******__TEST, BABY__******");
+
+})();
